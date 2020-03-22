bbbe89b75c9958d36ddf9531e6ceb3baaecd7416
Alexander <sanek.ryabov@mail.ru>
2020-03-22 14:28:49 +0300

    My first plots

--- Plot1.R
+++ Plot1.R
@@ -11,15 +11,16 @@ data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 data$Datetime <- as.POSIXct(paste(as.Date(data$Date), data$Time))
 
 #------   To get space for our Histogram and draw it  -------
+dev.new()
 par(mfcol = c(1,1))
 hist(data$Global_active_power, main="Global Active Power",
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 #------   Save our picture  -------
 dev.copy(png, file="plot1.png", height=480, width=480)
-
+dev.off()
 #------   Choose next screen  -------
-dev.set(dev.next())
+dev.new()
 
 #------   Draw our plot  -------
 par(mfcol = c(1,1))
@@ -28,11 +29,12 @@ plot(data$Global_active_power~data$Datetime, type="l",
 
 #------   Save our picture  -------
 dev.copy(png, file="plot2.png", height=480, width=480)
-
+dev.off()
 #------   Choose next screen  -------
-dev.set(dev.next())
+dev.new()
 
 #------   Draw next chart  -------
+par(mfcol = c(1,1))
 with(data, {
   plot(Sub_metering_1~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
@@ -46,9 +48,9 @@ legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
 
 #------   Save our picture  -------
 dev.copy(png, file="plot3.png", height=480, width=480)
-
+dev.off()
 #------   Choose next screen  -------
-dev.set(dev.next())
+dev.new()
 
 #------   Get space for our charts  -------
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
